43c9337d425a8ccf48f2f231c577faf0d938e167
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-05-09 21:01:12 +0200

    wip

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,7 @@
+const URL = `https://restcountries.com/v3.1/name/`
+
 const fetchCountries = (name) => {
-return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(
+return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`).then(
 Response => {
 if(Response.status === 404){
     return Promise.reject(new Error());
